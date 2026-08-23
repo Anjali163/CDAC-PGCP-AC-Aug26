d8e923fe6a823eeea75a76f1e661b3d33a3b6e40
Anjali Nanavare <154526342+Anjali163@users.noreply.github.com>
2026-08-23 08:59:37 +0530

    Add files via upload

---
+++ Assignment/Logic Building/LB_Assignment4/Numbers1toN.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Numbers1toN{
+	public static void main(String args[]){
+	
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter a number: ");
+	int N = sc.nextInt();
+	
+	for(int i = 1; i<=N; i++){
+		System.out.print(i);
+	
+	}
+ }
+
+} 
