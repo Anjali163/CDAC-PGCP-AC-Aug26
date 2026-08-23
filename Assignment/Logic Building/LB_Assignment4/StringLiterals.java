07d5cc7bdfd6c7f9267d32a906b7a2b22bc4eb9f
Anjali Nanavare <154526342+Anjali163@users.noreply.github.com>
2026-08-23 09:01:42 +0530

    Add files via upload

---
+++ Assignment/Logic Building/LB_Assignment4/StringLiterals.java
@@ -0,0 +1,13 @@
+class StringLiterals{
+    public static void main(String[] args) {
+
+       String str1 = "hello";
+       String str2 = "hello";
+
+         boolean result = str1 == str2;
+
+        System.out.println(
+            "Both variables point to the same object: " + result);
+        
+    }
+}
