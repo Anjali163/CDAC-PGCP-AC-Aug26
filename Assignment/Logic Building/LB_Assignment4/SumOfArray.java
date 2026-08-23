348f0ab8cb753c10257b6c3af7aa0cf3530c0724
Anjali Nanavare <154526342+Anjali163@users.noreply.github.com>
2026-08-23 09:03:04 +0530

    Add files via upload

---
+++ Assignment/Logic Building/LB_Assignment4/SumOfArray.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class SumOfArray{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[5];
+
+			System.out.println("Enter 5 integers:");
+
+        for (int i = 0; i < 5; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int sum = 0;
+
+        for (int x : arr) {
+            sum = sum + x;
+      }
+
+        System.out.println("The sum of all numbers is: " + sum);
+    }
+}
