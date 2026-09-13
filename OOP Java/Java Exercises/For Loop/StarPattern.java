5c1f4981196e287473c2bbb653e4c193d1071cae
Anjali Nanavare <154526342+Anjali163@users.noreply.github.com>
2026-09-13 23:16:11 +0530

    Add files via upload

---
+++ OOP Java/Java Exercises/For Loop/StarPattern.java
@@ -0,0 +1,30 @@
+package practise;
+
+
+public class StarPattern {
+
+	public static void main(String[] args) {
+		
+	/*  4. Using For loops write a program to display the following pattern
+		*
+		**
+		***
+		****
+		*****
+*/
+		
+		System.out.println("Star Pattern: ");
+		
+		for(int i=1; i<=5; i++) { 
+			
+			for(int j=1; j<=i; j++) {
+	
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+	}
+
+}
---
+++ OOP Java/Java Exercises/For Loop/evennumbers.java
@@ -0,0 +1,20 @@
+package practise;
+
+public class evennumbers {
+
+	public static void main(String[] args) {
+		
+        //2. using for loops write a Java Program to display all even numbers from 1 to 500
+		
+		int n=500;
+		System.out.println("The even numbers between 1 to 500 are: ");
+		
+		for(int i=0;i<=n; i++) {
+			if(i%2==0)
+				System.out.println(i);
+			
+		}
+		
+	}
+
+}
